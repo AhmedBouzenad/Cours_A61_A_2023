4df4a6aff7c294b233492c3230999bf45315b201
hmimid73 <ahmed.bouzenad@yahoo.ca>
2024-01-08 10:07:45 -0500

    Partie-4- Validation des données dans le modèle

---
+++ packages/regression_model/regression_model/test.py
@@ -0,0 +1,4 @@
+import regression_model  # repertoire qui contient des Modules construits
+import pathlib
+
+PACKAGE_ROOT = pathlib.Path(regression_model.__file__).resolve().parent
--- packages/regression_model/regression_model/train_pipeline.py
+++ packages/regression_model/regression_model/train_pipeline.py
@@ -5,7 +5,6 @@ from regression_model import pipeline
 from regression_model.processing.data_management import load_dataset, save_pipeline
 from regression_model.config import config
 
-
 def run_training() -> None:
     """Train the model."""
 
